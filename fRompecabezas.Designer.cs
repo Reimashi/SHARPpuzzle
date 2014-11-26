dea9e80aadb9c87c52987e4f6e9dc9dc64d226f3
Aitor González Fernández <reimashi@gmail.com>
2014-11-26 21:46:19 +0100

    Copia de proyecto local.

---
+++ fRompecabezas.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Rompecabezas
+{
+    partial class fRompecabezas
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.juegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoJuegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.botones = new System.Windows.Forms.Button[16];
+            for (int i = 0; i <= 15; i++)
+            {
+                this.botones[i] = new System.Windows.Forms.Button();
+            }
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 582);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1107, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(89, 17);
+            this.toolStripStatusLabel1.Text = "Movimientos: 0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.juegoToolStripMenuItem,
+            this.imagenToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1107, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // juegoToolStripMenuItem
+            // 
+            this.juegoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoJuegoToolStripMenuItem});
+            this.juegoToolStripMenuItem.Name = "juegoToolStripMenuItem";
+            this.juegoToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.juegoToolStripMenuItem.Text = "Juego";
+            // 
+            // nuevoJuegoToolStripMenuItem
+            // 
+            this.nuevoJuegoToolStripMenuItem.Name = "nuevoJuegoToolStripMenuItem";
+            this.nuevoJuegoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.nuevoJuegoToolStripMenuItem.Text = "Nuevo Juego";
+            this.nuevoJuegoToolStripMenuItem.Click += new System.EventHandler(this.NuevoJuego_Click);
+            // 
+            // imagenToolStripMenuItem
+            // 
+            this.imagenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1});
+            this.imagenToolStripMenuItem.Name = "imagenToolStripMenuItem";
+            this.imagenToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.imagenToolStripMenuItem.Text = "Imagen";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Paisaje",
+            "Cuna de Bebe",
+            "Dibujo de Montañas",
+            "Personalizada"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.SelectorImagen_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem1.Text = "Personalizar Imagen";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.PersonalizarImagen_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 27);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Nuevo Juego";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.NuevoJuego_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+            for (int i = 0; i <= 15; i++)
+            {
+                this.flowLayoutPanel1.Controls.Add(this.botones[i]);
+            }
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 56);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(510, 509);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // botones
+            // 
+            for (int i = 0; i < botones.Length; i++)
+            {
+                this.botones[i].Margin = new System.Windows.Forms.Padding(1);
+                this.botones[i].Name = "boton_" + i.ToString();
+                this.botones[i].Size = new System.Drawing.Size(125, 125);
+                this.botones[i].TabIndex = i;
+                this.botones[i].UseVisualStyleBackColor = true;
+                this.botones[i].Click += new System.EventHandler(this.Tablero_Click);
+            }
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(652, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 351);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 604);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Rompecabezas";
+            this.Icon = null;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem juegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoJuegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button[] botones;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ fRompecabezas.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rompecabezas
+{
+    public partial class fRompecabezas : Form
+    {
+        /* ========================= Definición de variables para la lógica del programa ===================================== */
+
+        private int[] estadoBotones = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        private Bitmap[] subimagenes = { null, null, null, null, null, null, null, null, null, null, null, null, null, null, null };
+        private int movimientos = 0;
+
+        public fRompecabezas()
+        {
+            InitializeComponent();
+            this.toolStripComboBox1.SelectedIndex = 0;
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aitor González Fernández\nhttp://github.com/Reimashi/SHARPpuzzle\nRompecabezas 0.1a (Rev.02)", "Acerca de...");
+        }
+
+        private void Tablero_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if (sender.Equals(botones[i]))
+                {
+                    if (moverFicha(i))
+                    {
+                        if (comprobarColocado())
+                        {
+                            this.toolStripStatusLabel1.Text = "Enhorabuena, has ganado";
+                            deshabilitarBotones();
+                        }
+                        else
+                        {
+                            this.toolStripStatusLabel1.Text = " Movimientos: " + movimientos;
+                        }
+                    }
+                    else
+                    {
+                        this.toolStripStatusLabel1.Text = " Movimiento incorrecto";
+                    }
+                }
+            }
+        }
+
+        private void SelectorImagen_Click(object sender, EventArgs e)
+        {
+            switch (this.toolStripComboBox1.SelectedIndex)
+            {
+                case 0:
+                    getImagenes(".\\images\\paisaje.jpg");
+                    break;
+
+                case 1:
+                    getImagenes(".\\images\\cuna.png");
+                    break;
+
+                case 2:
+                    getImagenes(".\\images\\montanas.png");
+                    break;
+
+                case 3:
+                    PersonalizarImagen_Click(sender, e);
+                    break;
+
+                default:
+                    getImagenes(".\\images\\paisaje.png");
+                    break;
+            }
+
+            nuevoJuego();
+        }
+
+        private void NuevoJuego_Click(object sender, EventArgs e)
+        {
+            nuevoJuego();
+        }
+
+        private void PersonalizarImagen_Click(object sender, EventArgs e)
+        {
+            cambiarImagen();
+        }
+
+        /* ====================================== Logica del programa ========================================================== */
+
+        //
+        // Metodo para cambiar de imagen con dialogo de seleccion
+        //
+        private void cambiarImagen()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Imagen (PNG)|*.png|Imagen (JPG)|*.jpg|Imagen (JPEG)|*.jpeg|Imagen (GIF)|*.gif|Imagen (BMP)|*.bmp|Todos|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    getImagenes(openFileDialog1.FileName);
+                    nuevoJuego();
+                    // Borrar la seleccion en el bar
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: No se puede leer el archivo:\n" + ex.Message, "Error!!!");
+                }
+            }
+        }
+
+
+        //
+        // Metodo que cambia la imagen y inicia un nuevo juego
+        //
+        private void cambiarImagen(String ruta)
+        {
+            getImagenes(ruta);
+            nuevoJuego();
+        }
+
+        //
+        // Metodo que revuelve los botones al azar
+        //
+        private void revolverBotones()
+        {
+            Random rnd = new Random();
+            int i = 0;
+            int j = 0;
+
+            for (i = 0; i < 15; i++)
+            {
+                estadoBotones[i] = rnd.Next(15);
+                for (j = 0; j < i; j++)
+                {
+                    if (estadoBotones[i] == estadoBotones[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+            estadoBotones[15] = 15;
+        }
+
+        //
+        // Metodo que inicia un juego nuevo
+        //
+        private void nuevoJuego()
+        {
+            int i = 0;
+            revolverBotones();
+
+            movimientos = 0;
+            this.toolStripStatusLabel1.Text = "Movimientos: " + movimientos.ToString();
+
+            if (subimagenes[i] == null)
+            {
+                deshabilitarBotones();
+                this.toolStripStatusLabel1.Text = " No hay ninguna imagen cargada";
+                return;
+            }
+
+            for (i = 0; i < 16; i++)
+            {
+                if (estadoBotones[i] != 15)
+                {
+                    botones[i].BackgroundImage = subimagenes[estadoBotones[i]];
+                    botones[i].Enabled = true;
+                }
+                else
+                {
+                    botones[i].BackgroundImage = null;
+                    botones[i].BackColor = Color.Black;
+                    botones[i].Enabled = false;
+                }
+            }
+        }
+
+        //
+        // Metodo que deshabilita todos los botones
+        //
+        private void deshabilitarBotones()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                botones[i].Enabled = false;
+            }
+        }
+
+        //
+        // Metodo que comprueba si el vector estadoBotones esta ordenado, es decir, comprueba si todas las imagenes están ordenadas.
+        //
+        private bool comprobarColocado()
+        {
+            int i = 0;
+
+            for (i = 0; i < 16; i++)
+            {
+                if (estadoBotones[i] != i)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //
+        // Metodo que repinta los botones en la posicion pasada por parametro
+        //
+        private void repintarBoton(int i)
+        {
+            if (estadoBotones[i] != 15)
+            {
+                botones[i].BackgroundImage = subimagenes[estadoBotones[i]];
+                botones[i].Enabled = true;
+            }
+            else
+            {
+                botones[i].BackColor = Color.Black;
+                botones[i].BackgroundImage = null;
+                botones[i].Enabled = false;
+            }
+        }
+
+        //
+        // Metodo que mueve una ficha
+        //
+        private bool moverFicha(int pos)
+        {
+            if (pos + 1 < 16)
+            {
+                if (estadoBotones[pos + 1] == 15 && ((pos + 1) % 4) != 0)
+                {
+                    estadoBotones[pos + 1] = estadoBotones[pos];
+                    estadoBotones[pos] = 15;
+
+                    repintarBoton(pos);
+                    repintarBoton(pos + 1);
+
+                    movimientos++;
+                    return true;
+                }
+
+                if (pos + 4 < 16)
+                {
+                    if (estadoBotones[pos + 4] == 15)
+                    {
+                        estadoBotones[pos + 4] = estadoBotones[pos];
+                        estadoBotones[pos] = 15;
+
+                        repintarBoton(pos);
+                        repintarBoton(pos + 4);
+
+                        movimientos++;
+                        return true;
+                    }
+                }
+            }
+
+
+            if (pos - 1 >= 0)
+            {
+                if (estadoBotones[pos - 1] == 15 && (pos - 1) / 4 == pos / 4)
+                {
+                    estadoBotones[pos - 1] = estadoBotones[pos];
+                    estadoBotones[pos] = 15;
+
+                    repintarBoton(pos);
+                    repintarBoton(pos - 1);
+
+                    movimientos++;
+                    return true;
+                }
+
+                if (pos - 4 >= 0)
+                {
+                    if (estadoBotones[pos - 4] == 15)
+                    {
+                        estadoBotones[pos - 4] = estadoBotones[pos];
+                        estadoBotones[pos] = 15;
+
+                        repintarBoton(pos);
+                        repintarBoton(pos - 4);
+
+                        movimientos++;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        //
+        // Metodo que redimensiona la imagen
+        //
+        private static Bitmap redimensionarImagen(Bitmap i, int width, int height)
+        {
+            // Se crea un nuevo bitmap con el tamaño final que le queremos asignar
+            Bitmap imagenBitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            imagenBitmap.SetResolution( Convert.ToInt32(i.HorizontalResolution), Convert.ToInt32(i.HorizontalResolution));
+            
+            // Se crea una zona de dibujado para el Bitmap
+            Graphics imagenGraphics = Graphics.FromImage(imagenBitmap);
+            
+            // Se establecen los parametros para la conversion
+            imagenGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            imagenGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            imagenGraphics.PixelOffsetMode =  System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+            // Se dibuja la nueva imagen
+            imagenGraphics.DrawImage( i, new Rectangle(0, 0, width, height), new Rectangle(0, 0, i.Width, i.Height), GraphicsUnit.Pixel);
+
+            return imagenBitmap;
+        }
+
+        //
+        // Metodo que obtiene y normaliza las imagenes
+        //
+        private Bitmap getImagen(String file)
+        {
+            try
+            {
+                Bitmap imagen = new Bitmap(file);
+                imagen = redimensionarImagen(imagen, 512, 512);
+                return imagen;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error!!!");
+                return null;
+            }
+        }
+
+        //
+        // Metodo para extraer partes de imagenes a partir de coordenadas y tamaños (Similar al metodo de redimensionar)
+        //
+        private Bitmap getSubimage(Bitmap ori, int cordx, int cordy, int tamw, int tamh)
+        {
+            // Se crea un nuevo bitmap con el tamaño final que le queremos asignar
+            Bitmap imagenBitmap = new Bitmap(tamw, tamh, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            imagenBitmap.SetResolution(Convert.ToInt32(ori.HorizontalResolution), Convert.ToInt32(ori.HorizontalResolution));
+
+            // Se crea una zona de dibujado para el Bitmap
+            Graphics imagenGraphics = Graphics.FromImage(imagenBitmap);
+
+            // Se establecen los parametros para la conversion
+            imagenGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            imagenGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            imagenGraphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+            // Se dibuja la nueva imagen
+            imagenGraphics.DrawImage(ori, new Rectangle(0, 0, tamw, tamh), new Rectangle(cordx, cordy, tamw, tamh), GraphicsUnit.Pixel);
+
+            return imagenBitmap;
+        }
+
+        //
+        // Metodo que obtiene 15 partes a partir de una imagen más grande
+        //
+        private bool getImagenes(String file)
+        {
+            Bitmap original = getImagen(file);
+
+            if (original == null)
+            {
+                return false;
+            }
+
+            // Se crean 15 segmentos con el metodo getSubimage( x, y, ancho, alto)
+            subimagenes[0] = getSubimage( original,  0, 0, original.Width / 4, original.Height / 4 );
+            subimagenes[1] = getSubimage( original,  (original.Width / 4) * 1, 0, original.Width / 4, original.Height / 4 );
+            subimagenes[2] = getSubimage( original,  (original.Width / 4) * 2, 0, original.Width / 4, original.Height / 4 );
+            subimagenes[3] = getSubimage( original,  (original.Width / 4) * 3, 0, original.Width / 4, original.Height / 4 );
+            subimagenes[4] = getSubimage( original,  0, (original.Height / 4) * 1, original.Width / 4, original.Height / 4 );
+            subimagenes[5] = getSubimage( original,  (original.Width / 4) * 1, (original.Height / 4) * 1, original.Width / 4, original.Height / 4 );
+            subimagenes[6] = getSubimage( original,  (original.Width / 4) * 2, (original.Height / 4) * 1, original.Width / 4, original.Height / 4 );
+            subimagenes[7] = getSubimage( original,  (original.Width / 4) * 3, (original.Height / 4) * 1, original.Width / 4, original.Height / 4 );
+            subimagenes[8] = getSubimage( original,  0, (original.Height / 4) * 2, original.Width / 4, original.Height / 4 );
+            subimagenes[9] = getSubimage( original,  (original.Width / 4) * 1, (original.Height / 4) * 2, original.Width / 4, original.Height / 4 );
+            subimagenes[10] = getSubimage( original,  (original.Width / 4) * 2, (original.Height / 4) * 2, original.Width / 4, original.Height / 4 );
+            subimagenes[11] = getSubimage( original,  (original.Width / 4) * 3, (original.Height / 4) * 2, original.Width / 4, original.Height / 4 );
+            subimagenes[12] = getSubimage( original,  0, (original.Height / 4) * 3, original.Width / 4, original.Height / 4 );
+            subimagenes[13] = getSubimage( original, (original.Width / 4) * 1, (original.Height / 4) * 3, original.Width / 4, original.Height / 4);
+            subimagenes[14] = getSubimage( original, (original.Width / 4) * 2, (original.Height / 4) * 3, original.Width / 4, original.Height / 4);
+
+            return true;
+        }
+    }
+}
